fb0f9a42549f04e9cd81a953eaa5595339c2d30e
Yoshan Mumberson <mryosh@outlook.com>
2024-09-26 21:44:55 +0100

    Removed export from workflow as it's no longer supported

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,8 @@
-// import bundleAnalyzer from '@next/bundle-analyzer';
-
-// // Setup bundle analyzer
-// const withBundleAnalyzer = bundleAnalyzer({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-
-// // Next.js configuration
-// const nextConfig = {
-//   reactStrictMode: false,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   experimental: {
-//     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-//   },
-//   output: 'export',
-//   basePath: '/Handball-Portfolio-Mantine',
-//   assetPrefix: '/Handball-Portfolio-Mantine/',
-// };
-
-// // Use ES module export syntax
-// export default withBundleAnalyzer(nextConfig);
-
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
   basePath: '/Handball-Portfolio-Mantine',
   // assetPrefix: '/Handball-Portfolio-Mantine/',
   reactStrictMode: false,
@@ -37,6 +12,7 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  output: 'export',
 }
  
 module.exports = nextConfig
