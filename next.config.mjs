1601ed89b14f6f2c52486f115ab57778a810859b
Yoshan Mumberson <mryosh@outlook.com>
2024-09-26 21:03:03 +0100

    Modified config to match docs

--- next.config.mjs
+++ next.config.mjs
@@ -14,13 +14,29 @@
 //   },
 // });
 
-import bundleAnalyzer from '@next/bundle-analyzer';
+// import bundleAnalyzer from '@next/bundle-analyzer';
 
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
+// const withBundleAnalyzer = bundleAnalyzer({
+//   enabled: process.env.ANALYZE === 'true',
+// });
 
-export default withBundleAnalyzer({
+// export default withBundleAnalyzer({
+//   reactStrictMode: false,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   experimental: {
+//     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+//   },
+//   output: 'export',  // Injecting output property
+// });
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
@@ -28,5 +44,15 @@ export default withBundleAnalyzer({
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
-  output: 'export',  // Injecting output property
-});
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
