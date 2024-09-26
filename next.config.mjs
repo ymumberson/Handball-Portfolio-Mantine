e440b6c8a034403db7a9c2f5a6741e3a6c484a45
Yoshan Mumberson <mryosh@outlook.com>
2024-09-26 21:19:24 +0100

    Modified config to match other project.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
 
+// Setup bundle analyzer
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-export default withBundleAnalyzer({
+// Next.js configuration
+const nextConfig = {
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
@@ -13,47 +15,9 @@ export default withBundleAnalyzer({
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
   output: 'export',
-});
+  basePath: '/Handball-Portfolio-Mantine',
+  assetPrefix: '/Handball-Portfolio-Mantine/',
+};
 
-// import bundleAnalyzer from '@next/bundle-analyzer';
-
-// const withBundleAnalyzer = bundleAnalyzer({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-
-// export default withBundleAnalyzer({
-//   reactStrictMode: false,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   experimental: {
-//     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-//   },
-//   output: 'export',  // Injecting output property
-// });
-
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: 'export',
-  
-//   reactStrictMode: false,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   experimental: {
-//     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-//   },
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-// }
- 
-// module.exports = nextConfig
+// Use ES module export syntax
+export default withBundleAnalyzer(nextConfig);
