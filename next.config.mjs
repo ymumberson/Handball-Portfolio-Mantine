82e057140ecd4a1922d552cc8a8f75e39db924d3
Yoshan Mumberson <mryosh@outlook.com>
2024-09-28 12:00:16 +0100

    Added mantine dependencies.

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,15 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  // basePath: '/Handball-Portfolio-Mantine',
+  basePath: '/Handball-Portfolio-Mantine',
   // assetPrefix: '/Handball-Portfolio-Mantine/',
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // experimental: {
-  //   optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  // },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
   output: 'export',
 }
  
